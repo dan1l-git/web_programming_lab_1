8fe80e98b3a009aa8dc05a533a0890811dc8edd7
dan1l-git <danilgavriluk756@gmail.com>
2025-03-12 14:38:00 +0200

    Added Manufacturer Class with basic logic.

---
+++ src/main/java/org/example/Manufacturer.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Manufacturer {
+    private String name;
+    private String country;
+
+    public Manufacturer(String name, String country) {
+        this.name = name;
+        this.country = country;
+    }
+
+    // get; set;
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public String getCountry() {return country;}
+    public void setCountry(String country) {this.country = country;}
+
+    @Override
+    public String toString() {
+        return "Manufacturer [name= " + name + ", country= " + country + "]";
+    }
+}
